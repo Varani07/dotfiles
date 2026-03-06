89872fb4d7c92bed65f0ac875f294b04c241381f
Matheus Cirne Varani <162613909+Varani07@users.noreply.github.com>
2026-03-06 08:35:22 -0300

    Removendo arquivos desnecessarios.

--- ryujinx/ryujinx_rodar_ryujinx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ryujinx_portable/publish
-DXVK_FRAME_RATE=45 firejail --noprofile --net=none prime-run ./Ryujinx
--- vm/rodar_vm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ~/archiso
-qemu-system-x86_64 -enable-kvm -m 2048 -smp 2 -cpu host -cdrom archlinux.iso -drive file=arch_disk.qcow2,format=qcow2 -boot d -net nic -net user -display gtk -audiodev none,id=noaudio
